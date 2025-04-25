8dd5f2e478faf4c9bdf42aa30284b76883579259
Rafael Borges Cunha <90654713+Killanovak@users.noreply.github.com>
2025-04-25 09:01:32 -0300

    Update otimizado_consumo_v2.29.py

--- otimizado_consumo_v2.29.py
+++ otimizado_consumo_v2.29.py
@@ -23,7 +23,7 @@ st.set_page_config(
 st.title("📊 Análise de Consumo de Energia")
 st.write("Uso de memória", f"{psutil.Process().memory_info().rss / (1024 * 1024):.1f} MB")
 st.sidebar.title("Analise de dados da API")
-st.sidebar.metric("Uso de memória", f"{psutil.Process().memory_info().rss / (1024 * 1024):.1f} MB")
+#st.sidebar.metric("Uso de memória", f"{psutil.Process().memory_info().rss / (1024 * 1024):.1f} MB")
 st.sidebar.write("Versão: 2.29")
 # ------- OTIMIZAÇÕES DE MEMÓRIA -------
 
