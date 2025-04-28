567dfdca4da766628b53060e2d3be599f7c4ef46
Rafael Borges Cunha <90654713+Killanovak@users.noreply.github.com>
2025-04-28 14:33:40 -0300

    Update otimizado_consumo_v2.29.py

--- otimizado_consumo_v2.29.py
+++ otimizado_consumo_v2.29.py
@@ -650,8 +650,8 @@ if st.button("Gerar Gráfico") and empresas_selecionadas:
             
             if dados_unidades:
                 tabela_unidades = pd.DataFrame(dados_unidades)
-                with st.expander("🏭 Ver Detalhamento por Unidade"):
-                    st.dataframe(tabela_unidades, hide_index=True)
+                st.title("🏭 Ver Detalhamento por Unidade")
+                st.dataframe(tabela_unidades, hide_index=True)
 
     
     # Liberar memória ao final
